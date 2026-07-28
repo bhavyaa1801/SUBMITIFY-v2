a60d2fce26eaf2eacc2c05edf1e5e2fef0f07426
Bhavya <rajputbhavya71@gmail.com>
2026-07-28 20:45:45 +0530

    speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
     <GuideProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </GuideProvider>
   </StrictMode>
 );
